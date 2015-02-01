547a7b67ba962336445391664715c189c9f037be
s9e <s9e.dev@gmail.com>
2015-02-01 04:11:29 +0100

    WiP

---
+++ tests/HelperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace s9e\SourceOptimizer\Tests;
+
+use PHPUnit_Framework_TestCase;
+use s9e\SourceOptimizer\Helper;
+use s9e\SourceOptimizer\TokenStream;
+
+class HelperTest extends PHPUnit_Framework_TestCase
+{
+}

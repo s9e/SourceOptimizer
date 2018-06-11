dcff5eae6997c14b63ebee06b824aa50d2dd531a
s9e <s9e.dev@gmail.com>
2018-06-11 02:48:11 +0200

    Updated copyright year [ci skip]

--- src/TokenStream.php
+++ src/TokenStream.php
@@ -2,7 +2,7 @@
 
 /**
 * @package   s9e\SourceOptimizer
-* @copyright Copyright (c) 2014-2017 The s9e Authors
+* @copyright Copyright (c) 2014-2018 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\SourceOptimizer;
